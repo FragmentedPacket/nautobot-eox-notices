aefc324ae90dba637117f4b7f414001ec4c8b044
Mikhail Yohman <mikhail.yohman@gmail.com>
2021-07-03 17:57:42 -0600

    Add UI tests. Use Nautobot core permissions

--- eox_notices/views.py
+++ eox_notices/views.py
@@ -1,6 +1,5 @@
 """Views for eox_notices."""
 
-from django.contrib.auth.mixins import PermissionRequiredMixin
 from nautobot.core.views.generic import (
     BulkDeleteView,
     BulkEditView,
@@ -15,69 +14,90 @@ from .forms import EoxNoticeForm, EoxNoticeBulkEditForm, EoxNoticeFilterForm
 from .filters import EoxNoticeFilter
 
 
-class EoxNoticesListView(PermissionRequiredMixin, ObjectListView):
+class EoxNoticesListView(ObjectListView):
     """List view."""
 
     queryset = EoxNotice.objects.prefetch_related("devices", "device_type")
     filterset = EoxNoticeFilter
     filterset_form = EoxNoticeFilterForm
     table = EoxNoticesTable
-    permission_required = "eox_notices.view_eoxnotice"
     action_buttons = ("add",)
 
+    def get_required_permission(self):
+        """Return required view permission."""
+        return "eox_notices.view_eoxnotice"
 
-class EoxNoticeView(PermissionRequiredMixin, ObjectView):
+
+class EoxNoticeView(ObjectView):
     """Detail view."""
 
     queryset = EoxNotice.objects.prefetch_related("devices", "device_type")
-    permission_required = "eox_notices.view_eoxnotice"
+
+    def get_required_permission(self):
+        """Return required view permission."""
+        return "eox_notices.view_eoxnotice"
 
 
-class EoxNoticeCreateView(PermissionRequiredMixin, ObjectEditView):
+class EoxNoticeCreateView(ObjectEditView):
     """Create view."""
 
     model = EoxNotice
     queryset = EoxNotice.objects.prefetch_related("devices", "device_type")
     model_form = EoxNoticeForm
-    permission_required = "eox_notices.add_eoxnotice"
     default_return_url = "plugins:eox_notices:eoxnotice_list"
 
+    def get_required_permission(self):
+        """Return required add permission."""
+        return "eox_notices.add_eoxnotice"
 
-class EoxNoticeDeleteView(PermissionRequiredMixin, ObjectDeleteView):
+
+class EoxNoticeDeleteView(ObjectDeleteView):
     """Delete view."""
 
     model = EoxNotice
     queryset = EoxNotice.objects.prefetch_related("devices", "device_type")
-    permission_required = "eox_notices.delete_eoxnotice"
     default_return_url = "plugins:eox_notices:eoxnotice_list"
 
+    def get_required_permission(self):
+        """Return required delete permission."""
+        return "eox_notices.delete_eoxnotice"
 
-class EoxNoticeEditView(PermissionRequiredMixin, ObjectEditView):
+
+class EoxNoticeEditView(ObjectEditView):
     """Edit view."""
 
     model = EoxNotice
     queryset = EoxNotice.objects.prefetch_related("devices", "device_type")
     model_form = EoxNoticeForm
-    permission_required = "eox_notices.change_eoxnotice"
     default_return_url = "plugins:eox_notices:eoxnotice"
 
+    def get_required_permission(self):
+        """Return required delete permission."""
+        return "eox_notices.change_eoxnotice"
 
-class EoxNoticeBulkDeleteView(PermissionRequiredMixin, BulkDeleteView):
+
+class EoxNoticeBulkDeleteView(BulkDeleteView):
     """View for deleting one or more EoxNotice records."""
 
-    permission_required = "eox_notices.delete_eoxnotice"
     queryset = EoxNotice.objects.prefetch_related("devices", "device_type")
     table = EoxNoticesTable
     bulk_delete_url = "plugins:eox_notices.eoxnotice_bulk_delete"
     default_return_url = "plugins:eox_notices:eoxnotice_list"
 
+    def get_required_permission(self):
+        """Return required delete permission."""
+        return "eox_notices.delete_eoxnotice"
 
-class EoxNoticeBulkEditView(PermissionRequiredMixin, BulkEditView):
+
+class EoxNoticeBulkEditView(BulkEditView):
     """View for editing one or more EoxNotice records."""
 
     queryset = EoxNotice.objects.prefetch_related("devices", "device_type")
     filterset = EoxNoticeFilter
     table = EoxNoticesTable
     form = EoxNoticeBulkEditForm
-    permission_required = "eox_notices.change_eoxnotice"
     bulk_edit_url = "plugins:eox_notices.eoxnotice_bulk_edit"
+
+    def get_required_permission(self):
+        """Return required change permission."""
+        return "eox_notices.change_eoxnotice"
