efb5d042cd102a98e4d74e136e27178fdefd1408
Mikhail Yohman <mikhail.yohman@gmail.com>
2021-07-03 00:21:24 -0600

    Revert some filter changes and tested/add bulk edit/delete for UI

--- eox_notices/views.py
+++ eox_notices/views.py
@@ -1,17 +1,24 @@
 """Views for eox_notices."""
 
 from django.contrib.auth.mixins import PermissionRequiredMixin
-from nautobot.core.views.generic import ObjectView, ObjectListView, ObjectEditView, ObjectDeleteView
+from nautobot.core.views.generic import (
+    BulkDeleteView,
+    BulkEditView,
+    ObjectDeleteView,
+    ObjectEditView,
+    ObjectListView,
+    ObjectView,
+)
 from .models import EoxNotice
 from .tables import EoxNoticesTable
-from .forms import EoxNoticeForm, EoxNoticeFilterForm
+from .forms import EoxNoticeForm, EoxNoticeBulkEditForm, EoxNoticeFilterForm
 from .filters import EoxNoticeFilter
 
 
 class EoxNoticesListView(PermissionRequiredMixin, ObjectListView):
     """List view."""
 
-    queryset = EoxNotice.objects.all()
+    queryset = EoxNotice.objects.prefetch_related("devices", "device_type").all()
     filterset = EoxNoticeFilter
     filterset_form = EoxNoticeFilterForm
     table = EoxNoticesTable
@@ -22,7 +29,7 @@ class EoxNoticesListView(PermissionRequiredMixin, ObjectListView):
 class EoxNoticeView(PermissionRequiredMixin, ObjectView):
     """Detail view."""
 
-    queryset = EoxNotice.objects.all()
+    queryset = EoxNotice.objects.prefetch_related("devices", "device_type")
     permission_required = "eox_notices.view_eoxnotice"
 
 
@@ -30,7 +37,7 @@ class EoxNoticeCreateView(PermissionRequiredMixin, ObjectEditView):
     """Create view."""
 
     model = EoxNotice
-    queryset = EoxNotice.objects.all()
+    queryset = EoxNotice.objects.prefetch_related("devices", "device_type")
     model_form = EoxNoticeForm
     permission_required = "eox_notices.add_eoxnotice"
     default_return_url = "plugins:eox_notices:eoxnotice_list"
@@ -40,7 +47,7 @@ class EoxNoticeDeleteView(PermissionRequiredMixin, ObjectDeleteView):
     """Delete view."""
 
     model = EoxNotice
-    queryset = EoxNotice.objects.all()
+    queryset = EoxNotice.objects.prefetch_related("devices", "device_type")
     permission_required = "eox_notices.delete_eoxnotice"
     default_return_url = "plugins:eox_notices:eoxnotice_list"
 
@@ -49,7 +56,28 @@ class EoxNoticeEditView(PermissionRequiredMixin, ObjectEditView):
     """Edit view."""
 
     model = EoxNotice
-    queryset = EoxNotice.objects.all()
+    queryset = EoxNotice.objects.prefetch_related("devices", "device_type")
     model_form = EoxNoticeForm
     permission_required = "eox_notices.change_eoxnotice"
     default_return_url = "plugins:eox_notices:eoxnotice"
+
+
+class EoxNoticeBulkDeleteView(PermissionRequiredMixin, BulkDeleteView):
+    """View for deleting one or more EoxNotice records."""
+
+    permission_required = "eox_notices.delete_eoxnotice"
+    queryset = EoxNotice.objects.prefetch_related("devices", "device_type")
+    table = EoxNoticesTable
+    bulk_delete_url = "plugins:eox_notices.eoxnotice_bulk_delete"
+    default_return_url = "plugins:eox_notices:eoxnotice_list"
+
+
+class EoxNoticeBulkEditView(PermissionRequiredMixin, BulkEditView):
+    """View for editing one or more EoxNotice records."""
+
+    queryset = EoxNotice.objects.prefetch_related("devices", "device_type")
+    filterset = EoxNoticeFilter
+    table = EoxNoticesTable
+    form = EoxNoticeBulkEditForm
+    permission_required = "eox_notices.change_eoxnotice"
+    bulk_edit_url = "plugins:eox_notices.eoxnotice_bulk_edit"
